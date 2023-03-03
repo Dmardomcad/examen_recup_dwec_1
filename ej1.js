39c7bd460ed48145b606b1d1a0b33c163cb78974
Dmardomcad <daniel.mariscal@iesrafaelalberti.es>
2023-03-03 09:11:52 +0100

    .

---
+++ ej1.js
@@ -0,0 +1,12 @@
+function calcularFactorial(numero){
+    //Se almacena el resultado en una variable
+    let resultado = 1
+    //Iteramos sobre el número y multiplicamos el resultado de la multiplicación por el siguiente número hasta llegar al introducido.
+    for (let num = 1; num <= numero; num++){
+        resultado *= num
+    }
+    return resultado
+}
+// Pedimos el número por pantalla
+let entrada = prompt("Numero")
+console.log(calcularFactorial(entrada))
---
+++ ej2.js
@@ -0,0 +1,10 @@
+const LETRAS = ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E']
+const resto = num%23
+
+function calcularDni(num){
+    if (num = 0 || num < 99999999){
+        console.log("Numero no valido")
+        return
+    }
+}
+console.log(calcularDni(9999999999))
