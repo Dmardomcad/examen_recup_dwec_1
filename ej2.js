423753b29abdad6563a37396eacef581a892bc3f
Dmardomcad <daniel.mariscal@iesrafaelalberti.es>
2023-03-03 09:44:28 +0100

    .

--- ej2.js
+++ ej2.js
@@ -3,17 +3,16 @@ function calcularDni(num){
     const LETRAS = ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E']
     const resto = num%23
     console.log(num)
-    if (num = 0 || num > 99999999){
+    if (num = 0 || num > 99999999 || isNaN(num)){
         console.log("Numero no valido")
         alert("Número no válido")
         return
     }
     let letraDni = LETRAS[resto]
-    console.log(letraDni)
     var dniCompleto = entrada + letraDni
     alert("El dni completo es "+ dniCompleto)
 }
 let entrada = prompt("Introduzca el número del DNI")
 calcularDni(entrada)
 
-// Se ha usado visual studio y console logs
+// Se ha usado el IDE visual studio code y console log para depurar
